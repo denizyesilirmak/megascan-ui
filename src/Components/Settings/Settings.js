267ec87d69686772765487f2a3a87abe6d6d17b1
denizyesilirmak <denizyesilirmak@gmail.com>
2019-12-10 17:39:56 +0300

    UPDATE: Navigator pagination

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,14 +1,82 @@
 import React, { Component } from 'react'
 import Navigator from './SettingsElements/Navigator'
+import SocketHelper from '../../SocketHelper'
 import './Settings.css'
 
-class Settings extends Component{
+class Settings extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      activeSettingTab: 0
+    }
 
-  render(){
-    return(
+    this.buttons = [
+      {
+        name: "Power"
+      },
+      {
+        name: "Time & Date"
+      },
+      {
+        name: "Storage"
+      },
+      {
+        name: "Connection"
+      },
+      {
+        name: "Security"
+      },
+      {
+        name: "Reset"
+      },
+      {
+        name: "Display"
+      },
+      {
+        name: "Language"
+      },
+      {
+        name: "Sound"
+      },
+      {
+        name: "Info"
+      },
+    ]
+  }
+
+  handleKeyDown = (socketData) => {
+    if (socketData.type !== 'button') { return }
+    let tempActiveSettingTab = this.state.activeSettingTab
+    switch (socketData.payload) {
+      case 'left':
+        if (tempActiveSettingTab > 0)
+          tempActiveSettingTab--
+        break
+      case 'right':
+        if (tempActiveSettingTab < this.buttons.length - 1)
+          tempActiveSettingTab++
+        break
+      case 'ok':
+
+        return
+      case 'back':
+
+        return
+      default:
+        break
+    }
+
+    this.setState({
+      activeSettingTab: tempActiveSettingTab
+    })
+  }
+
+  render() {
+    return (
       <div className="settings-component component">
-        <Navigator></Navigator>
+        <Navigator activeSettingTab={this.state.activeSettingTab} buttons={this.buttons}></Navigator>
+        <SocketHelper ref="socket" onMessage={this.handleKeyDown} />
       </div>
     )
   }
--- src/Components/Settings/SettingsElements/Navigator.js
+++ src/Components/Settings/SettingsElements/Navigator.js
@@ -4,27 +4,39 @@ import './Navigator.css'
 import LeftArrow from '../../../Assets/MenuIcons/left-arrow2.png'
 import RightArrow from '../../../Assets/MenuIcons/right-arrow2.png'
 
-class Navigator extends Component{
+class Navigator extends Component {
   // constructor(props){
   //   super(props)
   // }
 
-  render(){
-    return(
+  componentDidUpdate(prevProps) {
+    console.log("eskisi: " + prevProps.activeSettingTab % 3, "yenisi: " + this.props.activeSettingTab % 3)
+    if (prevProps.activeSettingTab % 3 === 2 && this.props.activeSettingTab % 3 === 0) {
+      console.log("sayfa sağ")
+      this.refs.naviSlider.style.transform = `translateX(${-1 * this.props.activeSettingTab * 220}px)`
+    }
+    else if (prevProps.activeSettingTab % 3 === 0 && this.props.activeSettingTab % 3 === 2) {
+      console.log("sayfa sol")
+      this.refs.naviSlider.style.transform = `translateX(${-1 * this.props.activeSettingTab * 480}px)`
+    }
+  }
+
+  render() {
+    return (
       <div className="navigator-component">
         <img alt="left" className="navigator-left-arrow" src={LeftArrow}></img>
         <img alt="right" className="navigator-right-arrow" src={RightArrow}></img>
 
-        <div className="navigator-buttons">
-          <div className="navigator-button">
-            Power Mode
-          </div>
-          <div className="navigator-button selected">
-            Time & Date
-          </div>
-          <div className="navigator-button">
-            STORAGE
-          </div>
+        <div className="navigator-buttons" ref="naviSlider">
+          {
+            this.props.buttons.map((e, k) => {
+              return (
+                <div key={k} className={`navigator-button ${this.props.activeSettingTab === k ? 'selected' : ''}`}>
+                  {e.name}
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     )

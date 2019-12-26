8e87a1f54bb33d0ad93cd04d417387ac145e3503
denizyesilirmak <denizyesilirmak@gmail.com>
2019-12-26 11:55:37 +0300

    UPDATE: Live Stream top part layout

--- src/Components/ScanModes/LiveStream/LiveStream.js
+++ src/Components/ScanModes/LiveStream/LiveStream.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './LiveStream.css'
 
-class LiveStrem extends Component{
-  render(){
-    return(
+class LiveStrem extends Component {
+  render() {
+    return (
       <div className="live-stream-component component">
+        <div className="live-stream-top">
+          <div className="stream-plot">
+            <canvas width="550" height="280">
 
+            </canvas>
+          </div>
+          <div className="stream-orientation">
+
+          </div>
+        </div>
+        <div className="live-stream-bottom">
+
+        </div>
       </div>
     )
   }

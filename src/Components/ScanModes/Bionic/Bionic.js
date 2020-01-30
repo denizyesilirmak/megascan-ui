819a8e1cf764c17b93d58f476f36e7a6dd655005
denizyesilirmak <denizyesilirmak@gmail.com>
2020-01-30 13:33:44 +0300

    ADDED: LineChart component to bionic scan

--- src/Components/ScanModes/Bionic/Bionic.js
+++ src/Components/ScanModes/Bionic/Bionic.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react'
 import './Bionic.css'
 
+import socketHelper from '../../../SocketHelper'
+
 import Depth_Icon from '../../../Assets/MenuIcons/icon-depth-2.png'
 import Save_Icon from '../../../Assets/MenuIcons/icon-save-outline.png'
 import Bionic_Rotator from '../../../Assets/MenuIcons/bionic-rotator.png'
 
+import LineChart from './LineChat'
 
-class Bionic extends Component{
 
-  componentDidMount(){
-
+class Bionic extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      sensorData: 0
+    }
   }
 
-  render(){
-    return(
+  componentDidMount() {
+    socketHelper.attach(this.handleKeyDown)
+
+    this.testInterval = setInterval(() => {
+      socketHelper.send('Q')
+    }, 60);
+  }
+
+  handleKeyDown = (socketData) => {
+    if (socketData.type === 'button') {
+      switch (socketData.payload) {
+        case 'left':
+          break
+        case 'right':
+
+          break
+        case 'down':
+
+          break
+        case 'up':
+
+          break
+        case 'ok':
+
+          return
+        case 'back':
+          clearInterval(this.testInterval);
+          setTimeout(() => {
+            this.props.navigateTo("menuScreen")
+          }, 250);
+        default:
+          break
+      }
+    }
+    else if (socketData.type === 'sensor') {
+      this.setState({
+        sensorData: socketData.payload
+      })
+    }
+  }
+
+  render() {
+    return (
       <div className="bionic component">
 
         <div className="b-button" id="depth-button">
@@ -27,14 +74,14 @@ class Bionic extends Component{
         </div>
 
         <div className="rotating-indicator-container">
-          <img ref="Rotator" className="rotator" src={Bionic_Rotator} alt="rotator"/>
+          <img ref="Rotator" className="rotator" src={Bionic_Rotator} alt="rotator" />
         </div>
 
         <div className="line-chart">
-
+          <LineChart value={this.state.sensorData} />
         </div>
 
-        
+
 
 
 
---
+++ src/Components/ScanModes/Bionic/LineChat.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class LineChart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sensorValue: 255
+    }
+
+    this.lines = new Array(37)
+    for (let index = 0; index < this.lines.length; index++) {
+      this.lines[index] = Math.trunc(Math.sin(index * 0.085) * this.state.sensorValue * (65 / 255))
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.lines)
+  }
+
+  componentDidUpdate() {
+    for (let index = 0; index < this.lines.length; index++) {
+      this.lines[index] = Math.trunc(Math.sin(index * 0.085) * this.props.value * (65 / 255) + Math.random() * 4)
+    }
+  }
+
+  render() {
+    return (
+      <svg width="600" height="80" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <linearGradient spreadMethod="pad" y2="1" x2="0" y1="0" x1="0" id="svg_38">
+            <stop offset="0" stopColor="#ffff00ff" />
+            <stop offset="1" stopColor="#00ff0000" />
+          </linearGradient>
+        </defs>
+        <g>
+
+          {
+            this.lines.map((e, i) => {
+              return (
+                <rect key={i} id={`svg_${i}`} height={e} width="10" y={7.5 + (65 - e)} x={11.8 + i * 16} fill="url(#svg_38)" />
+              )
+            })
+          }
+        </g>
+      </svg>
+    )
+  }
+}
+export default LineChart

6332143699b75480d2b3f8b743a5f3cb2a95ff40
denizyesilirmak <denizyesilirmak@gmail.com>
2020-01-31 17:35:25 +0300

    UPDATE: Num pad

--- src/Components/LockScreen/LockScreen.js
+++ src/Components/LockScreen/LockScreen.js
@@ -15,7 +15,16 @@ class LockScreen extends Component {
         <div className="lock-holder">
           <div className="pin"> 1453 </div>
           <div className="num-pad">
-            
+            <div className="key">1</div>
+            <div className="key">2</div>
+            <div className="key">3</div>
+            <div className="key">4</div>
+            <div className="key">5</div>
+            <div className="key">6</div>
+            <div className="key">7</div>
+            <div className="key">8</div>
+            <div className="key">9</div>
+            <div className="key">0</div>
           </div>
         </div>
       </div>

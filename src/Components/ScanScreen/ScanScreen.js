c225840b12c47a6717337efebfe441c5565668d8
denizyesilirmak <denizyesilirmak@gmail.com>
2020-02-25 15:30:52 +0300

    UPDATE: plot colors.

--- src/Components/ScanScreen/ScanScreen.js
+++ src/Components/ScanScreen/ScanScreen.js
@@ -10,16 +10,20 @@ const COLORS = {
     { pct: 67, color: { r: 0x00, g: 0xff, b: 0xff } },
     { pct: 127, color: { r: 0x00, g: 0xad, b: 0x00 } },
     { pct: 192, color: { r: 0xff, g: 0xff, b: 0x00 } },
-    { pct: 255, color: { r: 0xff, g: 0x00, b: 0x00 } }
+    { pct: 256, color: { r: 0xff, g: 0x00, b: 0x00 } }
   ]
 }
 
 class ScanScreen extends Component {
   constructor(props) {
     super(props)
-    this.y = 5
-    this.x = 5 * 4
+    this.y = 20
+    this.x = 10 * 4
+    this.total = 0
     this.counter = 0
+    this.average = 127
+    this.max = 0
+    this.min = 255
 
     this.state = {
       currentPoint: {
@@ -27,7 +31,8 @@ class ScanScreen extends Component {
         y: null
       },
       newLinePopup: false,
-      finishScanPopup: false
+      finishScanPopup: false,
+      finishPopupButtonIndex: true
     }
   }
 
@@ -53,13 +58,12 @@ class ScanScreen extends Component {
     this.initializeCamera();
 
     this.geometry = new THREE.PlaneGeometry(1, 1, this.x, this.y);
-    this.material = new THREE.MeshBasicMaterial({ wireframe: false, vertexColors: THREE.VertexColors });
+    this.material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: false, vertexColors: THREE.VertexColors });
 
     // console.log(this.matrix)
 
     this.dataInterval = setInterval(() => {
       this.requestSensorData()
-
     }, 200);
 
     this.graphMesh = new THREE.Mesh(this.geometry, this.material);
@@ -68,8 +72,14 @@ class ScanScreen extends Component {
     this.animate();
   }
 
+  // requestSensorData = () => {
+  //   if (!this.state.newLinePopup && !this.state.finishScanPopup) {
+  //     socketHelper.send('W')
+  //   }
+  // }
+
   requestSensorData = () => {
-    if (!this.state.newLinePopup && !this.state.finishScanPopup) {
+    if (true&& !this.state.finishScanPopup) {
       socketHelper.send('W')
     }
   }
@@ -79,8 +89,18 @@ class ScanScreen extends Component {
     if (socketData.type === 'button') {
       switch (socketData.payload) {
         case 'left':
+          if (this.state.finishScanPopup) {
+            this.setState({
+              finishPopupButtonIndex: !this.state.finishPopupButtonIndex
+            })
+          }
           break
         case 'right':
+          if (this.state.finishScanPopup) {
+            this.setState({
+              finishPopupButtonIndex: !this.state.finishPopupButtonIndex
+            })
+          }
           break
         case 'ok':
           if (this.state.newLinePopup === true && this.state.finishScanPopup === false) {
@@ -100,47 +120,78 @@ class ScanScreen extends Component {
           break
       }
     } else if (socketData.type === 'multipleSensor') {
+      let localSensorArray = [parseInt(socketData.payload[0]), parseInt(socketData.payload[1]), parseInt(socketData.payload[2]), parseInt(socketData.payload[3])]
+      let localMin = Math.min.apply(null, localSensorArray)
+      let localMax = Math.max.apply(null, localSensorArray)
+      this.min = localMin < this.min ? localMin : this.min
+      this.max = localMax > this.max ? localMax : this.max
+      console.log("max: ", this.max, "min: ", this.min, "avg: ", this.average)
 
-      this.zigzag("left")
+      this.zigzag("right")
       this.matrix[this.currentPoint.y][this.currentPoint.x] = [
-        parseInt(socketData.payload[0]) - parseInt((Math.random() * 20)),
-        parseInt(socketData.payload[0]) - parseInt((Math.random() * 20)),
-        parseInt(socketData.payload[0]) - parseInt((Math.random() * 20)),
-        parseInt(socketData.payload[0]) - parseInt((Math.random() * 20))]
+        localSensorArray[0],
+        localSensorArray[1],
+        localSensorArray[2],
+        localSensorArray[3]]
 
       for (let i = 0; i < this.matrix.length; i++) {
         for (let j = 0; j < this.matrix[i].length; j++) {
           for (let k = 0; k < this.matrix[i][j].length; k++) {
-            this.colorSquare((j * 4) + k, i, this.matrix[i][j][k])
+            this.colorSquare((j * 4) + k, i, this.matrix[i][j][k] - this.average)
           }
         }
       }
 
       this.counter++
+
+      this.total += (parseInt(socketData.payload[0]) + parseInt(socketData.payload[1]) + parseInt(socketData.payload[2]) + parseInt(socketData.payload[3])) / 4
+      this.average = Math.trunc(this.total / this.counter)
+      // console.log(this.average)
+
       if (this.counter === (this.x / 4) * this.y) {
         console.log(this.matrix)
         this.setState({
           finishScanPopup: true
         })
       }
-
     }
-
   }
 
 
   colorSquare = (x, y, c) => {
+
     let index = ((y) * 2 * this.x) + (x) * 2;
-    if (c === null) {
-      this.geometry.faces[index].color = new THREE.Color(0)
-      this.geometry.faces[index + 1].color = new THREE.Color(0)
-    } else {
-      this.geometry.faces[index].color = new THREE.Color(this.getColor(c))
-      this.geometry.faces[index + 1].color = new THREE.Color(this.getColor(c))
+    if(this.max - this.min> 6){
+      if (c > 0) {
+        this.geometry.faces[index].color = new THREE.Color(this.getColor(Math.trunc(this.map((c), 0, this.max, 127, 255))))
+        this.geometry.faces[index + 1].color = new THREE.Color(this.getColor(Math.trunc(this.map((c), 0, this.max, 127, 255))))
+      } else {
+        this.geometry.faces[index].color = new THREE.Color(this.getColor(Math.trunc(this.map((-1 * c), 0, this.min, 0, 127))))
+        this.geometry.faces[index + 1].color = new THREE.Color(this.getColor(Math.trunc(this.map((-1 * c), 0, this.min, 0, 127))))
+      }
     }
+    else{
+      if (c > 0) {
+        this.geometry.faces[index].color = new THREE.Color(this.getColor(Math.trunc(this.map(( 127 - c), 127, this.max, 127, 140))))
+        this.geometry.faces[index + 1].color = new THREE.Color(this.getColor(Math.trunc(this.map(( 127 - c), 127, this.max, 127, 140))))
+      } else {
+        this.geometry.faces[index].color = new THREE.Color(this.getColor(Math.trunc(this.map((-1 * c), 0, this.min, 100, 127))))
+        this.geometry.faces[index + 1].color = new THREE.Color(this.getColor(Math.trunc(this.map((-1 * c), 0, this.min, 100, 127))))
+      }
+    }
+
+
   }
 
+  map = (x, in_min, in_max, out_min, out_max) => {
+    return Math.abs(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
+
   getColor = (pct) => {
+    if(pct>255){
+      console.log(pct)
+    }
     for (var i = 1; i < COLORS.jet.length - 1; i++) {
       if (pct < COLORS.jet[i].pct) {
         break;
@@ -333,15 +384,23 @@ class ScanScreen extends Component {
           // Sadece Y degisiyor
           if (dir === 'up') {
             this.currentPoint.y--
+            if (this.currentPoint.y === 0) {
+              this.setState({
+                newLinePopup: true
+              })
+            }
           } else {
             this.currentPoint.y++
+            if (this.currentPoint.y === this.y - 1) {
+              this.setState({
+                newLinePopup: true
+              })
+            }
           }
         }
       }
     }
   }
-
-
 
   renderNewLinePopup = () => {
     return (
@@ -363,10 +422,10 @@ class ScanScreen extends Component {
             Scan is completed. Do you want to view scan result?
           </div>
           <div className="scan-screen-buttons">
-            <div className="scan-screen-button selected">
+            <div className={`scan-screen-button ${this.state.finishPopupButtonIndex === true ? "selected" : ""}`}>
               Okey
             </div>
-            <div className="scan-screen-button">
+            <div className={`scan-screen-button ${this.state.finishPopupButtonIndex === false ? "selected" : ""}`}>
               Cancel
             </div>
           </div>
@@ -380,7 +439,7 @@ class ScanScreen extends Component {
     return (
       <div className="scan-screen component">
         {
-          this.state.newLinePopup ? this.renderNewLinePopup() : ''
+          false? this.renderNewLinePopup() : ''
         }
 
         {
